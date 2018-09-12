e62efed4a663526eb783eb5158c9e09ccf4b61a4
CCTV-1 <script.tar.gz@gmail.com>
2018-09-12 17:20:57 +0800

    update script

--- distribution.sh
+++ distribution.sh
@@ -1,1 +1,13 @@
-strace ./MagicTower.exe | grep \ loaded\ D:\\\\MSYS2\\\\mingw64\\\\bin | sort | awk '{print $5}' | sed 's/\\/\//g' | xargs -i cp {} ./
+#!/bin/bash 
+set -e
+
+if [ $# != 1 ];then
+    exit 1;
+fi
+
+load_info=$(strace $1);
+load_info=$(grep "loaded D:\\\\MSYS2\\\\" <<< $load_info)
+load_info=$(sort <<< $load_info)
+load_info=$(awk '{print $5}' <<< $load_info)
+load_info=$(sed 's/\\/\//g' <<< $load_info)
+xargs -i cp {} ./ <<< $load_info
