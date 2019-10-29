8a8eba5560a5a373756206deb34923595b5dddb3
CCTV-1 <script.tar.gz@gmail.com>
2019-10-29 20:21:34 +0800

    move resource path macro definition

---
+++ src/resources.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+#define UI_DEFINE_RESOURCES_PATH "../resources/UI/magictower.ui"
+#define IMAGE_RESOURCES_PATH "../resources/images/"
+#define MUSIC_RESOURCES_PATH "../resources/music/"
+#define DATABSE_RESOURCES_PATH "../resources/database/magictower.db"
+#define CUSTOM_SCRIPTS_PATH "../resources/scripts/"
+
+#endif
