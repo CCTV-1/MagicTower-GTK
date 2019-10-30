0cba6904cb29eb182e93710df57658f37e23953c
CCTV-1 <script.tar.gz@gmail.com>
2019-10-30 22:29:43 +0800

    move hero defines to lua script

---
+++ resources/scripts/hero.lua
@@ -0,0 +1,15 @@
+hero_property=
+{
+    ["layers"] = 0,
+    ["x"] = 5,
+    ["y"] = 9,
+    ["level"] = 1,
+    ["life"] = 1000,
+    ["attack"] = 10,
+    ["defense"] = 10,
+    ["gold"] = 0,
+    ["experience"] = 0,
+    ["yellow_key"] = 0,
+    ["blue_key"] = 1,
+    ["red_key"] = 1
+}
